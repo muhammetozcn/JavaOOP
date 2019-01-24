f38dd42051bf909242254f920911f921121e8451
Muhammet Özcan <muhammetozcn34@gmail.com>
2019-01-24 21:52:07 +0300

    veritabanı
    Veritabanı metodları eklendi

--- JavaApplication2/src/javaapplication2/JavaApp.java
+++ JavaApplication2/src/javaapplication2/JavaApp.java
@@ -5,6 +5,14 @@
  */
 package javaapplication2;
 
+import com.mysql.jdbc.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mtozc
@@ -14,11 +22,50 @@ public class JavaApp {
     /**
      * @param args the command line arguments
      */
-   
+    static Connection conn = null;
+    private static Statement stmt;
+    public static ResultSet results = null;
+    public static String DB_url="jdbc:mysql://localhost:3306/kouceng";
+    public static String DB_user="root";
+    public static String DB_password="";
+    public static String driver = "com.mysql.jdbc.Driver";
+    
+    public ResultSet DB_query(String sql){
+        try{
+            Class.forName(driver).newInstance();
+            conn=DriverManager.getConnection(DB_url,DB_user,DB_password);
+            stmt=(Statement) conn.createStatement();
+            results=stmt.executeQuery(sql);
+            
+        }catch(Exception e){
+            e.printStackTrace();    
+        }
+        return results;
+    }
+    public void DB_update(String sql){
+          try{
+            Class.forName(driver).newInstance();
+            conn=DriverManager.getConnection(DB_url,DB_user,DB_password);
+            stmt=(Statement) conn.createStatement();
+            stmt.executeUpdate(sql);
+            
+        }catch(Exception e){
+            e.printStackTrace();    
+        }
+       
+        
+    }
+  
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!!");
         System.out.println("123");
-        
-        
+        ResultSet rs=new JavaApp().DB_query("SELECT *FROM classes");
+        try {
+            while(rs.next()){
+                System.out.println(rs.getString(""));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(JavaApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
 }}
